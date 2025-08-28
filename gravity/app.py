b9ca76f5320655d112283be95a6becd23f9fe7fe
Mohamed <mohamedboualamallah@icloud.com>
2025-08-28 07:03:33 +0300

    done

--- gravity/app.py
+++ gravity/app.py
@@ -34,11 +34,48 @@ neptune = Planet('neptune', 720, 100, 'gravity/planets/icon/neptune.png')
 
 planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
 
+mercurygravity = 3.7
+venusgravity = 8.87
+earthgravity = 9.81
+marsgravity = 3.71
+jupitergravity = 24.79
+saturngravity = 10.44
+uranusgravity = 8.69
+neptunegravity = 11.15
+
+class Ball:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vy = 0  # vertical velocity
+        self.radius = 15
+
+    def update(self, gravity):
+        self.vy += gravity * 0.1  # simulate gravity effect
+        self.y += self.vy
+
+        # Bounce off the ground
+        if self.y + self.radius > 600:
+            self.y = 600 - self.radius
+            self.vy = -self.vy * 0.7  # lose some energy on bounce
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255, 0, 0), (int(self.x), int(self.y)), self.radius)
+    def reset(self, x, y):
+        self.x = x
+        self.y = y
+        self.vy = 0
+ball = Ball(400, 50)
 while running:
     # ---- handle events ----
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            mode = 'setting'
+            bg = None
+            selected_planet = None
+            ball.reset(400, 50)
 
         if mode == 'setting' and event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = event.pos
@@ -64,6 +101,28 @@ while running:
     elif mode == 'running':
         if bg:
             screen.blit(bg, (0, 0))
+        
+        match selected_planet:
+            case 'mercury':
+                gravity = mercurygravity
+            case 'venus':
+                gravity = venusgravity
+            case 'earth':
+                gravity = earthgravity
+            case 'mars':
+                gravity = marsgravity
+            case 'jupiter':
+                gravity = jupitergravity
+            case 'saturn':
+                gravity = saturngravity
+            case 'uranus':
+                gravity = uranusgravity
+            case 'neptune':
+                gravity = neptunegravity
+        
+        ball.update(gravity)
+        ball.draw(screen)
+        
 
     pygame.display.flip()
     clock.tick(60)
